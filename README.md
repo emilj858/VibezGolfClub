db8d53df28fbfb62ffa11ba49f1aa55a491ff92a
Emil Juboori <Emil@MacBook-Pro-3.local>
2021-09-17 15:36:35 -0700

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+Command to run sass
+sass --watch scss/main.scss css/style.css
---
+++ carousel.js
@@ -0,0 +1,39 @@
+carousel = (function() {
+    var box = document.querySelector(".reviews-container");
+    var next = box.querySelector(".next");
+    var prev = box.querySelector(".prev");
+    var items = box.querySelectorAll(".content li");
+    var counter = 0;
+    var amount = items.length;
+    var current = items[0];
+    //box.classList.add("active");
+
+    function navigate(direction) {
+        current.classList.remove("current");
+        counter = counter + direction;
+
+        if (direction === -1 && counter < 0) {
+            counter = amount - 1;
+        }
+        if (direction === 1 && !items[counter]) {
+            counter = 0;
+        }
+        current = items[counter];
+        current.classList.add("current");
+        
+    };
+
+    let timing = setInterval(function() {
+        navigate(1);
+    }, 5000);
+
+    next.addEventListener("click", function(ev) {
+        navigate(1);
+    });
+    prev.addEventListener("click", function(ev) {
+        navigate(-1);
+        clearInterval(timing);
+    });
+    
+    navigate(0);
+})();
